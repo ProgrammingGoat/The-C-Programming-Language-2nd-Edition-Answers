67570bdb499c8e5847384e07f34b629f3e0d8074
Lukas Brackmann <brackmann.lukas@gmail.com>
2024-09-03 13:11:44 +0200

    Exercises 1-9

---
+++ Chapter1/exercise-1-1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+/* Run the "hello world" program on your system. Experiment with leaving out parts of the program, to see what error messages you get. */
+
+int main() {
+    printf("hello, world\n");
+}
---
+++ Chapter1/exercise-1-7.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+/* Write a program to print the value of EOF. */
+
+int main() {
+    printf("Value of EOF: %d", EOF);
+}
